216798da1045321e2852a9c9a7d81cf1e0812efc
pyhrishi <mail.sarodehrishi21@gmail.com>
2022-12-11 08:15:15 +0530

    Navigation added but seems buggy. Fix needed.

---
+++ js/main.js
@@ -0,0 +1,28 @@
+/* Menu Variables */
+const navMenu = document.getElementById("nav-menu");
+navToggle = document.getElementById("nav-toggle");
+navClose = document.getElementById("nav-close");
+
+/* Show Menu */
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    });
+}
+
+/* Hide Menu */
+if(navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
+}
+
+/*  */
+
+/*  */
+
+/*  */
+
+/*  */
+
+/*  */
